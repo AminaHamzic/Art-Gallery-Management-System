61b399aed0b655ead8861b6a2292511ef9d59360
ahamzic4 <60984054+ahamzic4@users.noreply.github.com>
2023-03-24 23:15:17 +0100

    second commit

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
- require 'vendor/autoload.php';
-
- Flight::route('/', function(){
-    echo 'amina';
- });
 
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
+require 'vendor/autoload.php';
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=lab3_db','root',''));
+Flight::route('GET /api/users', function(){
+   $users = Flight::db()->query('SELECT * FROM Users', PDO::FETCH_ASSOC)->fetchAll();
+   var_dump($users);
+   Flight::json($users);
+});
 Flight::start();
+
+// Database credentials
+$host = 'localhost'; // assuming the database is on the same server
+$username = 'root';
+$password = '';
+$dbname = 'lab3_db';
+
+
+
 ?>
